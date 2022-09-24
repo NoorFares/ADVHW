b3f6c49c79ab45d654fd3dd9cf8404f5205b2931
git config --global user.namNoorFares <s12028537@stu.najah.edu>
2022-09-24 20:04:53 +0300

    student class

---
+++ Student.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class Student extends Person{
+    //constructor
+    public Student(String id, String name, LocalDate birthDate){
+        super(id, name, birthDate);
+    }
+}
+
